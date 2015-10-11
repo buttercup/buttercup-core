4714369e0d75773dd95a48d980c26a5455e20e4a
Perry Mitchell <perry@perrymitchell.net>
2015-10-12 00:02:50 +0300

    Re-exposure of some classes. Alpha version.

--- source/module.js
+++ source/module.js
@@ -4,19 +4,17 @@
 
 	GLOBAL.root = __dirname;
 
-	// Encryption info from: http://lollyrock.com/articles/nodejs-encryption/
-	var Encryption = require(GLOBAL.root + "/encryption/encrypt.js"),
-		Decryption = require(GLOBAL.root + "/encryption/decrypt.js");
-
 	module.exports = {
 
-		decryptText: function(text, password) {
-			return Decryption.decrypt(text, password);
-		},
+		Dataset:  			require(GLOBAL.root + "/classes/Dataset.js"),
+		VaultGroup: 		require(GLOBAL.root + "/classes/VaultGroup.js"),
+		VaultEntry:  		require(GLOBAL.root + "/classes/VaultEntry.js"),
 
-		encryptText: function(text, password) {
-			return Encryption.encrypt(text, password);
-		}
+		FileDatasource: 	require(GLOBAL.root + "/classes/FileDatasource.js"),
+
+		// Encryption info from: http://lollyrock.com/articles/nodejs-encryption/
+		Encryption: require(GLOBAL.root + "/encryption/encrypt.js"),
+		Decryption: require(GLOBAL.root + "/encryption/decrypt.js")
 
 	};
 
