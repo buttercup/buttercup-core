2473d49a10095b438b4b563ff79e0b87d09846f5
Perry Mitchell <perry@perrymitchell.net>
2015-12-10 22:10:58 +0200

    Added compress/decompress tools

--- source/tools/encoding.js
+++ source/tools/encoding.js
@@ -3,9 +3,40 @@
 	"use strict";
 
 	var Crypto = require("crypto"),
-		uuid = require('uuid');
+		uuid = require("uuid"),
+		gzip = require("gzip-js");
+
+	var __gzipOptions = {
+		level: 9,
+		timestamp: parseInt(Date.now() / 1000, 10)
+	};
 
 	var lib = module.exports = {
+
+		compress: function(text) {
+			var compressed = gzip.zip(text, __gzipOptions),
+				compressedLength = compressed.length,
+				outputText = "";
+			for (var i = 0; i < compressedLength; i += 1) {
+				outputText += String.fromCharCode(compressed[i]);
+			}
+			return outputText;
+		},
+
+		decompress: function(text) {
+			var compressedData = [],
+				textLen = text.length;
+			for (var i = 0; i < textLen; i += 1) {
+				compressedData.push(text.charCodeAt(i));
+			}
+			var decompressedData = gzip.unzip(compressedData),
+				decompressedLength = decompressedData.length,
+				outputText = "";
+			for (var j = 0; j < decompressedLength; j += 1) {
+				outputText += String.fromCharCode(decompressedData[j]);
+			}
+			return outputText;
+		},
 
 		escapeTextValue: function(txt) {
 			return txt.replace(/"/g, '\\"');
