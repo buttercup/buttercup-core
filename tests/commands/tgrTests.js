d05ffb55d73e3966d84924299e96246104b2905e
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-02-17 12:49:03 +0000

    Add tests around title setting for tgr command

--- tests/commands/tgrTests.js
+++ tests/commands/tgrTests.js
@@ -18,5 +18,51 @@ module.exports = {
       }, 'Group not found for ID', 'An error was thrown when no group found');
       test.done();
     }
+  },
+
+  titleSet: {
+    titleSetCorrectlyForBla: function (test) {
+      var expectedTitle = 'Bla';
+      var command = new tgr();
+
+      var fakeGroup = {
+        title: ''
+      };
+
+      var fakeSearching = {
+        findGroupByID: function (a, b) {
+          return fakeGroup;
+        }
+      };
+
+      command.injectSearching(fakeSearching);
+
+      command.execute({ }, 1, expectedTitle);
+
+      test.strictEqual(fakeGroup.title, expectedTitle, 'The title was set to ' + expectedTitle + ' correctly');
+      test.done();
+    },
+
+    titleSetCorrectlyForJames: function (test) {
+      var expectedTitle = 'James';
+      var command = new tgr();
+
+      var fakeGroup = {
+        title: ''
+      };
+
+      var fakeSearching = {
+        findGroupByID: function (a, b) {
+          return fakeGroup;
+        }
+      };
+
+      command.injectSearching(fakeSearching);
+
+      command.execute({ }, 1, expectedTitle);
+
+      test.strictEqual(fakeGroup.title, expectedTitle, 'The title was set to ' + expectedTitle + ' correctly');
+      test.done();
+    }
   }
 };
