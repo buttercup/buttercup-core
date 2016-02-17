665cf3ad4235befb89c1f414239bf041896fac75
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-02-17 12:45:50 +0000

    Add test around group not being found

--- tests/commands/tgrTests.js
+++ tests/commands/tgrTests.js
@@ -1,12 +1,22 @@
 var tgr = require("__buttercup/classes/commands/command.tgr.js");
-/*
+
 module.exports = {
-  failMe: {
-    failmeplease: function(test) {
-      console.log(tgr);
-      test.fail();
+  errors: {
+    groupNotFoundThrowsError: function (test) {
+      var command = new tgr();
+
+      var fakeSearching = {
+        findGroupByID: function (a, b) {
+          return false;
+        }
+      };
+
+      command.injectSearching(fakeSearching);
+
+      test.throws(function() {
+        command.execute({ }, 1, 'a');
+      }, 'Group not found for ID', 'An error was thrown when no group found');
       test.done();
     }
   }
 };
-*/
