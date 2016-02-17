3dd4c6da2972a6358c2fc6938b9ea25090eea15e
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-02-17 13:04:18 +0000

    Test cen command

--- tests/commands/cenTests.js
+++ tests/commands/cenTests.js
@@ -22,4 +22,97 @@ module.exports = {
       test.done();
     }
   },
+
+  entry: {
+    oneEntryWhenInitiallyEmpty: function (test) {
+      var fakeGroup = {
+        entries: []
+      };
+
+      var fakeSearching = {
+        findGroupByID: function (a, b) {
+          return fakeGroup;
+        }
+      };
+
+      this.command.injectSearching(fakeSearching);
+
+      this.command.execute({ }, 1, 1);
+
+      test.strictEqual(fakeGroup.entries.length, 1, 'One entry when initially empty');
+      test.done();
+    },
+
+    twoEntriesWhenInitiallyOne: function (test) {
+      var fakeGroup = {
+        entries: [{
+          id: 1,
+    			title: "a test entry"
+        }]
+      };
+
+      var fakeSearching = {
+        findGroupByID: function (a, b) {
+          return fakeGroup;
+        }
+      };
+
+      this.command.injectSearching(fakeSearching);
+
+      this.command.execute({ }, 1, 2);
+
+      test.strictEqual(fakeGroup.entries.length, 2, 'Two entries when initially one');
+      test.done();
+    },
+
+    expectedIdWhenGiven1: function (test) {
+      var expectedId = 1;
+      var fakeGroup = {
+        entries: []
+      };
+
+      var fakeSearching = {
+        findGroupByID: function (a, b) {
+          return fakeGroup;
+        }
+      };
+
+      var pushedId;
+      fakeGroup.entries.push = function (entry) {
+        pushedId = entry.id;
+      };
+
+      this.command.injectSearching(fakeSearching);
+
+      this.command.execute({ }, 1, expectedId);
+
+      test.strictEqual(pushedId, expectedId, 'Entry pushed with expected id of ' + expectedId);
+      test.done();
+    },
+
+    expectedIdWhenGiven50: function (test) {
+      var expectedId = 50;
+      var fakeGroup = {
+        entries: []
+      };
+
+      var fakeSearching = {
+        findGroupByID: function (a, b) {
+          return fakeGroup;
+        }
+      };
+
+      var pushedId;
+      fakeGroup.entries.push = function (entry) {
+        pushedId = entry.id;
+      };
+
+      this.command.injectSearching(fakeSearching);
+
+      this.command.execute({ }, 1, expectedId);
+
+      test.strictEqual(pushedId, expectedId, 'Entry pushed with expected id of ' + expectedId);
+      test.done();
+    }
+  }
 };
