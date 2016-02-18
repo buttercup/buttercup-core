d5ce77f587c22193553b1b25c028deb55d1dd969
Perry Mitchell <perry@perrymitchell.net>
2016-02-18 18:37:53 +0200

    Test archive generation

---
+++ scripts/generate_test_archive.js
@@ -0,0 +1,29 @@
+(function() {
+
+    "use strict";
+
+    var packageJSON = require(__dirname + "/../package.json"),
+        path = require("path"),
+        outputDir = path.resolve(__dirname + "/../tests/_archives"),
+        outputFile = outputDir + "/test-archive-" + packageJSON.version + ".bcup",
+        Buttercup = require(__dirname + "/../source/module.js");
+
+    console.log("Building archive...");
+
+    // Archive
+    var archive = new Buttercup.Archive(),
+        mainGroup = archive.createGroup("test-group-main"),
+        mainEntry = mainGroup.createEntry("test-entry-main");
+
+    mainEntry.setProperty("username", "user123한@test.рф");
+    mainEntry.setProperty("password", "* পাসওয়ার্ড! ");
+    mainEntry.setMeta("test-meta", "test-value 8");
+
+    // Datasource
+    console.log("Saving archive: " + outputFile);
+    var datasource = new Buttercup.FileDatasource(outputFile);
+    datasource.save(archive, "this is a long password used for a test archive!");
+
+    console.log("Test archive completed for version: " + packageJSON.version);
+
+})();
