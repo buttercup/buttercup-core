2ccd27fe27cda78853ecb1a3d6a67bd9c109725e
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-02-23 13:50:02 +0000

    Check error is thrown if group not found

--- tests/commands/sgaTests.js
+++ tests/commands/sgaTests.js
@@ -4,5 +4,29 @@ module.exports = {
   setUp: function(cb) {
     this.command = new sga();
     (cb)();
-  }
+  },
+
+  errors: {
+    groupNotFoundForId: function(test) {
+      var fakeSearching = {
+        findGroupByID: function(a, b) {
+          return false;
+        }
+      };
+
+      this.command.injectSearching(fakeSearching);
+
+      var errorThrown = false;
+      try {
+        this.command.execute({ }, 0, '', '');
+      } catch (e) {
+        if (e.message === 'Group not found for ID') {
+          errorThrown = true;
+        }
+      } finally {
+        test.strictEqual(errorThrown, true, 'Error thrown');
+        test.done();
+      }
+    }
+  },
 };
