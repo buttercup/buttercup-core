302a72bf6d74d2c08120198ee9b7d8cdab6071b6
Perry Mitchell <perry@perrymitchell.net>
2016-02-27 00:06:58 +0200

    Tests tidyup final

--- tests/commands/tgrTests.js
+++ tests/commands/tgrTests.js
@@ -1,71 +1,75 @@
 var tgr = require("__buttercup/classes/commands/command.tgr.js");
 
 module.exports = {
-  setUp: function(cb) {
-    this.command = new tgr();
-    (cb)();
-  },
-
-  errors: {
-    groupNotFoundThrowsError: function (test) {
-      var fakeSearching = {
-        findGroupByID: function (a, b) {
-          return false;
-        }
-      };
-
-      this.command.injectSearching(fakeSearching);
-
-      test.throws(function() {
-        this.command.execute({ }, 1, 'a');
-      }.bind(this), 'Group not found for ID', 'An error was thrown when no group found');
-      test.done();
-    }
-  },
-
-  titleSet: {
-    titleSetCorrectlyForBla: function (test) {
-      var expectedTitle = 'Bla';
-      var command = new tgr();
-
-      var fakeGroup = {
-        title: ''
-      };
-
-      var fakeSearching = {
-        findGroupByID: function (a, b) {
-          return fakeGroup;
-        }
-      };
-
-      this.command.injectSearching(fakeSearching);
-
-      this.command.execute({ }, 1, expectedTitle);
-
-      test.strictEqual(fakeGroup.title, expectedTitle, 'The title was set to ' + expectedTitle + ' correctly');
-      test.done();
+    setUp: function(cb) {
+        this.command = new tgr();
+        (cb)();
     },
 
-    titleSetCorrectlyForJames: function (test) {
-      var expectedTitle = 'James';
-      var command = new tgr();
+    errors: {
 
-      var fakeGroup = {
-        title: ''
-      };
+        testGroupNotFoundThrowsError: function (test) {
+            var fakeSearching = {
+                findGroupByID: function (a, b) {
+                    return false;
+                }
+            };
 
-      var fakeSearching = {
-        findGroupByID: function (a, b) {
-          return fakeGroup;
+            this.command.injectSearching(fakeSearching);
+
+            test.throws(function() {
+                this.command.execute({ }, 1, 'a');
+            }.bind(this), 'Group not found for ID', 'An error was thrown when no group found');
+            test.done();
         }
-      };
 
-      command.injectSearching(fakeSearching);
+    },
 
-      command.execute({ }, 1, expectedTitle);
+    titleSet: {
 
-      test.strictEqual(fakeGroup.title, expectedTitle, 'The title was set to ' + expectedTitle + ' correctly');
-      test.done();
+        testTitleSetCorrectlyForBla: function (test) {
+            var expectedTitle = 'Bla';
+            var command = new tgr();
+
+            var fakeGroup = {
+                title: ''
+            };
+
+            var fakeSearching = {
+                findGroupByID: function (a, b) {
+                    return fakeGroup;
+                }
+            };
+
+            this.command.injectSearching(fakeSearching);
+
+            this.command.execute({ }, 1, expectedTitle);
+
+            test.strictEqual(fakeGroup.title, expectedTitle, 'The title was set to ' + expectedTitle + ' correctly');
+            test.done();
+        },
+
+        testTitleSetCorrectlyForJames: function (test) {
+            var expectedTitle = 'James';
+            var command = new tgr();
+
+            var fakeGroup = {
+                title: ''
+            };
+            
+            var fakeSearching = {
+                findGroupByID: function (a, b) {
+                    return fakeGroup;
+                }
+            };
+
+            command.injectSearching(fakeSearching);
+
+            command.execute({ }, 1, expectedTitle);
+
+            test.strictEqual(fakeGroup.title, expectedTitle, 'The title was set to ' + expectedTitle + ' correctly');
+            test.done();
+        }
+
     }
-  }
 };
