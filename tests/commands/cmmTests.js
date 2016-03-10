9bd5383e43b70cae1b26a0556194f3d8eeee9830
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-03-10 13:48:06 +0000

    Test that it calls to callback

--- tests/commands/cmmTests.js
+++ tests/commands/cmmTests.js
@@ -4,5 +4,20 @@ module.exports = {
     setUp: function(cb) {
         this.command = new cmm();
         (cb)();
+    },
+
+    callbackInjected: {
+        callsToCallback: function(test) {
+            var callbackCalled = false;
+            var callback = function(comment) {
+                callbackCalled = true;
+            };
+
+            this.command.injectCommentCallback(callback);
+            this.command.execute({}, "");
+
+            test.strictEqual(callbackCalled, true, "Calls into callback");
+            test.done();
+        }
     }
 };
