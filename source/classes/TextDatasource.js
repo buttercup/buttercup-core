a099c55d2575e015bad08381aab4ea64b4fd4f21
Perry Mitchell <perry@perrymitchell.net>
2016-07-16 14:01:53 +0300

    Datasources upgraded to ES6 classes with inheritance

--- source/classes/TextDatasource.js
+++ source/classes/TextDatasource.js
@@ -8,41 +8,79 @@
         signing = require("../tools/signing.js"),
         encoding = require("../tools/encoding.js");
 
-    var TextDatasource = function(content) {
-        this._content = content;
-    };
+    /**
+     * Datasource for text input and output
+     * @class TextDatasource
+     */
+    class TextDatasource {
 
-    TextDatasource.prototype.load = function(password) {
-        return Promise.resolve(this._content).then(function(data) {
-            if (!signing.hasValidSignature(data)) {
-                return Promise.reject("No valid signature in archive");
-            }
-            return signing.stripSignature(data);
-        }).then(function(encryptedData) {
-            return iocane.decryptWithPassword(encryptedData, password);
-        }).then(function(decrypted) {
-            if (decrypted && decrypted.length > 0) {
-                var decompressed = encoding.decompress(decrypted);
-                if (decompressed) {
-                    return decompressed.split("\n");
-                }
-            }
-            return Promise.reject("Decryption failed");
-        }).then(function(history) {
-            var archive = new Archive(),
-                westley = archive._getWestley();
-            westley.clear();
-            history.forEach(westley.execute.bind(westley));
-            return archive;
-        });
-    };
+        /**
+         * Constructor for the text datasource
+         * @param {string} content The content to load from
+         */
+        constructor(content) {
+            this._content = content;
+        }
 
-    TextDatasource.prototype.save = function(archive, password) {
-        var history = archive._getWestley().getHistory().join("\n"),
-            compressed = encoding.compress(history);
-        return iocane.encryptWithPassword(compressed, password)
-            .then(signing.sign);
-    };
+        /**
+         * Load from the stored content using a password to decrypt
+         * @param {string} password The password to decrypt with
+         * @returns {Promise.<Archive>} A promise that resolves with an open archive
+         */
+        load(password) {
+            return Promise.resolve(this._content)
+                .then(function(data) {
+                    if (!signing.hasValidSignature(data)) {
+                        return Promise.reject("No valid signature in archive");
+                    }
+                    return signing.stripSignature(data);
+                })
+                .then(function(encryptedData) {
+                    return iocane.decryptWithPassword(encryptedData, password);
+                })
+                .then(function(decrypted) {
+                    if (decrypted && decrypted.length > 0) {
+                        var decompressed = encoding.decompress(decrypted);
+                        if (decompressed) {
+                            return decompressed.split("\n");
+                        }
+                    }
+                    return Promise.reject("Decryption failed");
+                })
+                .then(function(history) {
+                    var archive = new Archive(),
+                        westley = archive._getWestley();
+                    westley.clear();
+                    history.forEach(westley.execute.bind(westley));
+                    return archive;
+                });
+        }
+
+        /**
+         * Save an archive with a password
+         * @param {Archive} archive The archive to save
+         * @param {string} password The password to encrypt with
+         * @returns {Promise.<string>} A promise resolving with the encrypted content
+         */
+        save(archive, password) {
+            var history = archive._getWestley().getHistory().join("\n"),
+                compressed = encoding.compress(history);
+            return iocane
+                .encryptWithPassword(compressed, password)
+                .then(signing.sign);
+        }
+
+        /**
+         * Set the text content
+         * @param {string} content The new text content
+         * @returns {TextDatasource} Self
+         */
+        setContent(content) {
+            this._content = content;
+            return this;
+        }
+
+    }
 
     module.exports = TextDatasource;
 
--- source/classes/WebDAVDatasource.js
+++ source/classes/WebDAVDatasource.js
@@ -5,53 +5,81 @@
     var webdavFS = require("webdav-fs"),
         TextDatasource = require("./TextDatasource.js");
 
-    var WebDAV = function(endpoint, path, username, password) {
-        var endpointLen = endpoint.length;
-        endpoint = (endpoint[endpointLen - 1] === "/") ? endpoint : endpoint + "/";
-        path = (path[0] === "/") ? path : "/" + path;
-        this._wfs = webdavFS(endpoint, username, password);
-        this._path = path;
-    };
+    /**
+     * WebDAV datasource for reading and writing remote archives
+     * @class WebDAVDatasource
+     * @augments TextDatasource
+     */
+    class WebDAVDatasource extends TextDatasource {
 
-    WebDAV.prototype.load = function(password) {
-        var wfs = this._wfs,
-            path = this._path;
-        return (new Promise(function(resolve, reject) {
-            wfs.readFile(path, "utf8", function(error, data) {
-                if (error) {
-                    (reject)(error);
-                } else {
-                    (resolve)(data);
-                }
+        /**
+         * Constructor for the datasource
+         * @param {string} endpoint URL for the WebDAV service (without resource path)
+         * @param {string} webDAVPath Resource path on the WebDAV service
+         * @param {string} username Username for the WebDAV service
+         * @param {string} password Password for the WebDAV service
+         */
+        constructor(endpoint, webDAVPath, username, password) {
+            var endpointLen = endpoint.length;
+            endpoint = (endpoint[endpointLen - 1] === "/") ? endpoint : endpoint + "/";
+            webDAVPath = (webDAVPath[0] === "/") ? webDAVPath : "/" + webDAVPath;
+            this._wfs = webdavFS(endpoint, username, password);
+            this._path = webDAVPath;
+            super("");
+        }
+
+        /**
+         * Load the archive using a password
+         * @param {string} password The password for archive decryption
+         * @returns {Promise.<Archive>} A promise resolving with the opened archive
+         */
+        load(password) {
+            var wfs = this._wfs,
+                filePath = this._path;
+            return (new Promise(function(resolve, reject) {
+                wfs.readFile(filePath, "utf8", function(error, data) {
+                    if (error) {
+                        (reject)(error);
+                    } else {
+                        (resolve)(data);
+                    }
+                });
+            })).then((content) => {
+                this.setContent(content);
+                return super.load(password);
+            }).catch(function(err) {
+                var errorMsg = "Failed opening archive: " + err;
+                console.error(errorMsg);
+                throw err;
             });
-        })).then(function(content) {
-            var textDatasource = new TextDatasource(content);
-            return textDatasource.load(password);
-        }).catch(function(err) {
-            var errorMsg = "Failed opening archive: " + err;
-            console.error(errorMsg);
-            throw new Error(errorMsg);
-        });
-    };
+        }
 
-    WebDAV.prototype.save = function(archive, password) {
-        var textDatasource = new TextDatasource(),
-            wfs = this._wfs,
-            path = this._path;
-        return textDatasource.save(archive, password)
-            .then(function(encrypted) {
-                return new Promise(function(resolve, reject) {
-                    wfs.writeFile(path, encrypted, "utf8", function(error) {
-                        if (error) {
-                            (reject)(error);
-                        } else {
-                            (resolve)();
-                        }
+        /**
+         * Save an archive with a password to the WebDAV service
+         * @param {Archive} archive The archive to save
+         * @param {string} password The password for encryption
+         * @returns {Promise} A promise resolving when the save is complete
+         */
+        save(archive, password) {
+            var wfs = this._wfs,
+                filePath = this._path;
+            return super
+                .save(archive, password)
+                .then(function(encrypted) {
+                    return new Promise(function(resolve, reject) {
+                        wfs.writeFile(filePath, encrypted, "utf8", function(error) {
+                            if (error) {
+                                (reject)(error);
+                            } else {
+                                (resolve)();
+                            }
+                        });
                     });
                 });
-            });
-    };
+        }
 
-    module.exports = WebDAV;
+    }
+
+    module.exports = WebDAVDatasource;
 
 })(module);
