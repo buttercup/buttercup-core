53deda631d9087c5635e5a52f6633558fd87b0a4
Perry Mitchell <perry@perrymitchell.net>
2016-08-07 18:56:29 +0300

    Remove let in datasource tools tests

--- tests/datasourceTests.js
+++ tests/datasourceTests.js
@@ -9,7 +9,7 @@ module.exports = {
     extractDSStrProps: {
 
         testExtractsProperties: function(test) {
-            let props = "ds=text,something=1,another-one=something.else,empty=",
+            var props = "ds=text,something=1,another-one=something.else,empty=",
                 extracted = datasourceTools.extractDSStrProps(props);
             test.strictEqual(extracted.ds, "text", "Should extract datasource type correctly");
             test.strictEqual(extracted.something, "1", "Should extract basic properties");
