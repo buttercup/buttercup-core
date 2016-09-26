183d8fe9ef83e92c3b7e2c33c9e60fc357355b2d
Perry Mitchell <perry@perrymitchell.net>
2016-09-26 22:16:24 +0300

    Add tests for new keyfile support in TextDatasource

--- tests/TextDatasourceTests.js
+++ tests/TextDatasourceTests.js
@@ -1,20 +1,34 @@
-var lib = require("../source/module.js");
+"use strict";
+
+const lib = require("../source/module.js");
+
+const path = require("path");
 
 var Archive = lib.Archive,
     TextDatasource = lib.TextDatasource,
+    Credentials = lib.Credentials,
     signing = lib.tools.signing,
     datasourceTools = lib.tools.datasource;
+
+const binFilePath = path.resolve(__dirname, "./_helpers/test.bin");
 
 module.exports = {
 
     setUp: function(cb) {
-        var _this = this;
         this.archive = new Archive();
         var group = this.archive.createGroup("main");
         var datasource = new TextDatasource();
         datasource.save(this.archive, "abc123")
-            .then(function(data) {
-                _this.content = data;
+            .then((data) => {
+                this.contentFromPassword = data;
+                return datasource.save(this.archive, new Credentials({ keyfile: binFilePath }));
+            })
+            .then((data) => {
+                this.contentFromKeyfile = data;
+                return datasource.save(this.archive, new Credentials({ password: "abc123", keyfile: binFilePath }));
+            })
+            .then((data) => {
+                this.contentFromBoth = data;
             })
             .then(cb)
             .catch(function(err) {
@@ -24,9 +38,35 @@ module.exports = {
 
     load: {
 
-        loadsFromContent: function(test) {
-            var tds = new TextDatasource(this.content);
+        loadsFromContentWithOnlyPassword: function(test) {
+            var tds = new TextDatasource(this.contentFromPassword);
             tds.load("abc123")
+                .then(function(archive) {
+                    test.ok(archive instanceof Archive, "Should return an archive");
+                    test.strictEqual(archive.getGroups()[0].getTitle(), "main", "Should contain correct group");
+                    test.done();
+                })
+                .catch(function(err) {
+                    console.error(err);
+                });
+        },
+
+        loadsFromContentWithOnlyKeyfile: function(test) {
+            var tds = new TextDatasource(this.contentFromKeyfile);
+            tds.load(new Credentials({ keyfile: binFilePath }))
+                .then(function(archive) {
+                    test.ok(archive instanceof Archive, "Should return an archive");
+                    test.strictEqual(archive.getGroups()[0].getTitle(), "main", "Should contain correct group");
+                    test.done();
+                })
+                .catch(function(err) {
+                    console.error(err);
+                });
+        },
+
+        loadsFromContentWithBothPasswordAndKeyfile: function(test) {
+            var tds = new TextDatasource(this.contentFromBoth);
+            tds.load(new Credentials({ password: "abc123", keyfile: binFilePath }))
                 .then(function(archive) {
                     test.ok(archive instanceof Archive, "Should return an archive");
                     test.strictEqual(archive.getGroups()[0].getTitle(), "main", "Should contain correct group");
