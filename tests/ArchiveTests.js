69f17baf852112f0b4002c70622af4292b4aa42d
Perry Mitchell <perry@perrymitchell.net>
2016-10-15 15:03:41 +0300

    Removed deprecated method containsGroupWithTitle - fixes #67

--- tests/ArchiveTests.js
+++ tests/ArchiveTests.js
@@ -1,42 +1,43 @@
 var lib = require("../source/module.js"),
-	encoding = require("../source/tools/encoding.js"),
-	Archive = lib.Archive,
-	ManagedGroup = lib.ManagedGroup;
+    encoding = require("../source/tools/encoding.js");
+    
+var Archive = lib.Archive,
+    ManagedGroup = lib.ManagedGroup;
 
 module.exports = {
 
-	setUp: function(cb) {
-		var archiveA = new Archive(),
-			mainGroupID = encoding.getUniqueID(),
-			secondaryGroupID = encoding.getUniqueID(),
-			entry1ID = encoding.getUniqueID(),
-			thirdGroupID = encoding.getUniqueID(),
-			commonCommands = [
-				'cgr 0 ' + mainGroupID,
-				'tgr ' + mainGroupID + ' "Main Group"',
-				'pad ' + encoding.getUniqueID(),
-				'cgr ' + mainGroupID + ' ' + secondaryGroupID,
-				'tgr ' + secondaryGroupID + ' "Secondary Group"',
-				'pad ' + encoding.getUniqueID(),
-				'cen ' + mainGroupID + ' ' + entry1ID,
-				'sep ' + entry1ID + ' title "My first entry"',
-				'pad ' + encoding.getUniqueID(),
-				'sep ' + entry1ID + ' username "anonymous"',
-				'sep ' + entry1ID + ' password "retro"',
-				'pad ' + encoding.getUniqueID(),
-				'cmm "after pad"',
-				'cgr 0 ' + thirdGroupID,
-				'tgr ' + thirdGroupID + ' "Websites"',
-				'pad ' + encoding.getUniqueID()
-			];
-		commonCommands.forEach(function(command) {
-			archiveA._getWestley().execute(command);
-		});
+    setUp: function(cb) {
+        var archiveA = new Archive(),
+            mainGroupID = encoding.getUniqueID(),
+            secondaryGroupID = encoding.getUniqueID(),
+            entry1ID = encoding.getUniqueID(),
+            thirdGroupID = encoding.getUniqueID(),
+            commonCommands = [
+                'cgr 0 ' + mainGroupID,
+                'tgr ' + mainGroupID + ' "Main Group"',
+                'pad ' + encoding.getUniqueID(),
+                'cgr ' + mainGroupID + ' ' + secondaryGroupID,
+                'tgr ' + secondaryGroupID + ' "Secondary Group"',
+                'pad ' + encoding.getUniqueID(),
+                'cen ' + mainGroupID + ' ' + entry1ID,
+                'sep ' + entry1ID + ' title "My first entry"',
+                'pad ' + encoding.getUniqueID(),
+                'sep ' + entry1ID + ' username "anonymous"',
+                'sep ' + entry1ID + ' password "retro"',
+                'pad ' + encoding.getUniqueID(),
+                'cmm "after pad"',
+                'cgr 0 ' + thirdGroupID,
+                'tgr ' + thirdGroupID + ' "Websites"',
+                'pad ' + encoding.getUniqueID()
+            ];
+        commonCommands.forEach(function(command) {
+            archiveA._getWestley().execute(command);
+        });
 
-		this.archiveA = archiveA;
-		this.entry1ID = entry1ID;
-		this.group2ID = secondaryGroupID;
-		this.group3ID = thirdGroupID;
+        this.archiveA = archiveA;
+        this.entry1ID = entry1ID;
+        this.group2ID = secondaryGroupID;
+        this.group3ID = thirdGroupID;
 
         this.archiveB = new Archive();
         this.archiveBgroup1 = this.archiveB.createGroup("parent");
@@ -52,27 +53,27 @@ module.exports = {
         archiveBChild1Entry2.setProperty("password", "terrific");
         archiveBChild1Entry2.setMeta("some meta", "1986");
 
-		cb();
-	},
+        cb();
+    },
 
-	createWithDefaults: {
+    createWithDefaults: {
 
-		testCreatesGeneralGroup: function(test) {
-			var archive = Archive.createWithDefaults(),
-				firstGroup = archive.getGroups()[0];
-			test.strictEqual(firstGroup.getTitle(), "General", "First group should be 'General'");
-			test.done();
-		},
+        testCreatesGeneralGroup: function(test) {
+            var archive = Archive.createWithDefaults(),
+                firstGroup = archive.getGroups()[0];
+            test.strictEqual(firstGroup.getTitle(), "General", "First group should be 'General'");
+            test.done();
+        },
 
-		testCreatesTrashGroup: function(test) {
-			var archive = Archive.createWithDefaults(),
-				firstGroup = archive.getGroups()[1];
-			test.strictEqual(firstGroup.getTitle(), "Trash", "First group should be 'Trash'");
-			test.strictEqual(firstGroup.isTrash(), true, "First group should be of type 'trash'");
-			test.done();
-		}
+        testCreatesTrashGroup: function(test) {
+            var archive = Archive.createWithDefaults(),
+                firstGroup = archive.getGroups()[1];
+            test.strictEqual(firstGroup.getTitle(), "Trash", "First group should be 'Trash'");
+            test.strictEqual(firstGroup.isTrash(), true, "First group should be of type 'trash'");
+            test.done();
+        }
 
-	},
+    },
 
     findEntriesByMeta: {
 
@@ -171,96 +172,74 @@ module.exports = {
 
     },
 
-	getEntryByID: {
+    getEntryByID: {
 
-		testGetsEntryIfExists: function(test) {
-			var entry = this.archiveA.getEntryByID(this.entry1ID);
-			test.strictEqual(entry.getProperty("title"), "My first entry");
-			test.strictEqual(entry.getProperty("username"), "anonymous");
-			test.strictEqual(entry.getProperty("password"), "retro");
-			test.done();
-		},
+        testGetsEntryIfExists: function(test) {
+            var entry = this.archiveA.getEntryByID(this.entry1ID);
+            test.strictEqual(entry.getProperty("title"), "My first entry");
+            test.strictEqual(entry.getProperty("username"), "anonymous");
+            test.strictEqual(entry.getProperty("password"), "retro");
+            test.done();
+        },
 
-		testGetsNullIfNotFound: function(test) {
-			var entry = this.archiveA.getEntryByID("999");
-			test.strictEqual(entry, null, "Entry should not exist");
-			test.done();
-		}
+        testGetsNullIfNotFound: function(test) {
+            var entry = this.archiveA.getEntryByID("999");
+            test.strictEqual(entry, null, "Entry should not exist");
+            test.done();
+        }
 
-	},
+    },
 
-	getGroupByID: {
+    getGroupByID: {
 
-		testGetsGroupIfExists: function(test) {
-			var group = this.archiveA.getGroupByID(this.group2ID);
-			test.strictEqual(group.getTitle(), "Secondary Group");
-			test.done();
-		},
+        testGetsGroupIfExists: function(test) {
+            var group = this.archiveA.getGroupByID(this.group2ID);
+            test.strictEqual(group.getTitle(), "Secondary Group");
+            test.done();
+        },
 
-		testGetsNullIfNotFound: function(test) {
-			var group = this.archiveA.getGroupByID("999");
-			test.strictEqual(group, null, "Group should not exist");
-			test.done();
-		}
+        testGetsNullIfNotFound: function(test) {
+            var group = this.archiveA.getGroupByID("999");
+            test.strictEqual(group, null, "Group should not exist");
+            test.done();
+        }
 
-	},
+    },
 
-	getGroups: {
+    getGroups: {
 
-		testGetsAll: function(test) {
-			var groups = this.archiveA.getGroups();
-			test.strictEqual(groups.length, 2, "Archive root should contain 2 groups");
-			test.done();
-		},
+        testGetsAll: function(test) {
+            var groups = this.archiveA.getGroups();
+            test.strictEqual(groups.length, 2, "Archive root should contain 2 groups");
+            test.done();
+        },
 
-		testGroupTypes: function(test) {
-			var groups = this.archiveA.getGroups();
-			groups.forEach(function(group) {
-				test.ok(group instanceof ManagedGroup, "Groups should be ManagedGroup instances");
-			});
-			test.done();
-		}
+        testGroupTypes: function(test) {
+            var groups = this.archiveA.getGroups();
+            groups.forEach(function(group) {
+                test.ok(group instanceof ManagedGroup, "Groups should be ManagedGroup instances");
+            });
+            test.done();
+        }
 
-	},
+    },
 
-	getTrashGroup: {
+    getTrashGroup: {
 
-		testReturnsNullIfNotPresent: function(test) {
-			var trashGroup = this.archiveA.getTrashGroup();
-			test.strictEqual(trashGroup, null, "No trash group should be present");
-			test.done();
-		},
+        testReturnsNullIfNotPresent: function(test) {
+            var trashGroup = this.archiveA.getTrashGroup();
+            test.strictEqual(trashGroup, null, "No trash group should be present");
+            test.done();
+        },
 
-		testReturnsGroupIfPresent: function(test) {
-			var group = this.archiveA.getGroupByID(this.group3ID);
-			group.setAttribute(ManagedGroup.Attributes.Role, "trash");
-			var trashGroup = this.archiveA.getTrashGroup();
-			test.strictEqual(trashGroup.getID(), group.getID(), "Trash group should be present");
-			test.done();
-		}
+        testReturnsGroupIfPresent: function(test) {
+            var group = this.archiveA.getGroupByID(this.group3ID);
+            group.setAttribute(ManagedGroup.Attributes.Role, "trash");
+            var trashGroup = this.archiveA.getTrashGroup();
+            test.strictEqual(trashGroup.getID(), group.getID(), "Trash group should be present");
+            test.done();
+        }
 
-	},
-
-	containsGroupWithTitle: {
-
-		testReturnsTrueForMainGroup: function(test) {
-			var hasMainGroup = this.archiveA.containsGroupWithTitle('Main Group');
-			test.strictEqual(hasMainGroup, true, "Archive should have group 'Main Group'");
-			test.done();
-		},
-
-		testReturnsTrueForSecondaryGroup: function(test) {
-			var hasSecondaryGroup = this.archiveA.containsGroupWithTitle('Secondary Group');
-			test.strictEqual(hasSecondaryGroup, true, "Archive should have group 'Secondary Group'");
-			test.done();
-		},
-
-		testReturnsFalseForNonExistentTestGroup: function(test) {
-			var hasNonExistentTestGroup = this.archiveA.containsGroupWithTitle('Non Existent Test Group');
-			test.strictEqual(hasNonExistentTestGroup, false, "Archive should not have group 'Non Existent Test Group'");
-			test.done();
-		}
-
-	}
+    }
 
 };
