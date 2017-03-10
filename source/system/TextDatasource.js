181234cdcf19846b6e4124a687a2b91b3052197d
Perry Mitchell <perry@perrymitchell.net>
2017-03-10 21:41:05 +0200

    Add breaking test for encoding

--- source/system/TextDatasource.js
+++ source/system/TextDatasource.js
@@ -130,7 +130,7 @@ class TextDatasource {
 
     /**
      * Set the text content
-     * @param {string} content The new text content
+     * @param {String} content The encrypted text content
      * @returns {TextDatasource} Self
      */
     setContent(content) {
