89379232e6c0220abdafbf6e304ed1272ae3ffe4
Perry Mitchell <perry@perrymitchell.net>
2017-04-28 15:49:50 +0300

    Add docs

--- source/system/storage/MemoryStorageInterface.js
+++ source/system/storage/MemoryStorageInterface.js
@@ -9,10 +9,19 @@ class MemoryStorageInterface extends StorageInterface {
         this._store = {};
     }
 
+    /**
+     * Get all keys from storage
+     * @returns {Promise.<Array.<String>>} A promise that resolves with an array of keys
+     */
     getAllKeys() {
         return Promise.resolve(Object.keys(this._store));
     }
 
+    /**
+     * Get the value of a key
+     * @param {String} name The key name
+     * @returns {Promise.<String>} A promise that resolves with the value
+     */
     getValue(name) {
         const value = this._store.hasOwnProperty(name) ?
             this._store[name] :
@@ -20,6 +29,12 @@ class MemoryStorageInterface extends StorageInterface {
         return Promise.resolve(value);
     }
 
+    /**
+     * Set the value for a key
+     * @param {String} name The key name
+     * @param {String} value The value to set
+     * @returns {Promise} A promise that resolves when the value is set
+     */
     setValue(name, value) {
         this._store[name] = value;
         return Promise.resolve();
