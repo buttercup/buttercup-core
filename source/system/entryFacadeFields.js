a67f5c5fa73b46f2f32450b384e83adb7bc1acc5
Perry Mitchell <perry@perrymitchell.net>
2017-05-15 16:58:10 +0300

    Add credit card facade

--- source/system/entryFacadeFields.js
+++ source/system/entryFacadeFields.js
@@ -14,6 +14,78 @@ function createBaseFields(entry) {
     ];
 }
 
+function createCreditCardFields(entry) {
+    return [
+        ...createBaseFields(entry),
+        createFieldDescriptor(
+            entry,
+            "Card Holder",
+            "property",
+            "username"
+        ),
+        createFieldDescriptor(
+            entry,
+            "Card Number",
+            "property",
+            "password",
+            {
+                formatting: {
+                    placeholder: "XXXX-XXXX-XXXX-XXXX",
+                    vendor: {
+                        delimiter: "-",
+                        blocks: [4, 4, 4, 4],
+                        numericOnly: true
+                    }
+                }
+            }
+        ),
+        createFieldDescriptor(
+            entry,
+            "Card Type",
+            "meta",
+            "type"
+        ),
+        createFieldDescriptor(
+            entry,
+            "CVV",
+            "meta",
+            "cvv"
+        ),
+        createFieldDescriptor(
+            entry,
+            "Valid From",
+            "meta",
+            "valid_from",
+            {
+                formatting: {
+                    placeholder: "MM/YYYY",
+                    vendor: {
+                        delimiter: "/",
+                        blocks: [2, 4],
+                        numericOnly: true
+                    }
+                }
+            }
+        ),
+        createFieldDescriptor(
+            entry,
+            "Expiry",
+            "meta",
+            "expiry",
+            {
+                formatting: {
+                    placeholder: "MM/YYYY",
+                    vendor: {
+                        delimiter: "/",
+                        blocks: [2, 4],
+                        numericOnly: true
+                    }
+                }
+            }
+        )
+    ];
+}
+
 function createLoginFields(entry) {
     return [
         ...createBaseFields(entry),
@@ -65,6 +137,7 @@ function createWebsiteFields(entry) {
 }
 
 module.exports = {
+    credit_card: createCreditCardFields,
     login: createLoginFields,
     ssh_key: createSSHKeyFields,
     website: createWebsiteFields
