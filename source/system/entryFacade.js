a91b0bdae13a12c2e104864508f721844f50544f
Perry Mitchell <perry@perrymitchell.net>
2017-07-24 19:42:12 +0300

    Add docs for meta addition method

--- source/system/entryFacade.js
+++ source/system/entryFacade.js
@@ -2,6 +2,15 @@ const Entry = require("./Entry.js");
 const facadeFieldFactories = require("./entryFacadeFields.js");
 const { createFieldDescriptor } = require("../tools/entry.js");
 
+/**
+ * Add meta fields to a fields array that are not mentioned in a preset
+ * Facades are creaded by presets which don't mention all meta values (custom user
+ * added items). This method adds the unmentioned items to the facade fields so that
+ * they can be edited as well.
+ * @param {Entry} entry An Entry instance
+ * @param {Array.<EntryFacadeField>} fields An array of fields
+ * @returns {Array.<EntryFacadeField>} A new array with all combined fields
+ */
 function addMetaFieldsNonDestructive(entry, fields) {
     const exists = metaName => fields.find(item => item.field === "meta" && item.property === metaName);
     const meta = entry.toObject().meta || {};
