4ed1df0e422115722f6406447d7e122a0bfa4fd0
Perry Mitchell <perry@perrymitchell.net>
2017-10-21 19:01:46 +0300

    Add queued saves for Workspace

---
+++ test/integration/Workspace.spec.js
@@ -0,0 +1,22 @@
+describe("Workspace", function() {
+
+    beforeEach(function() {
+        this.archive = Archive.createWithDefaults();
+        this.ds = new TextDatasource("");
+        this.workspace = new Workspace();
+        this.workspace.setPrimaryArchive(this.archive, this.ds, createCredentials.fromPassword("testing"));
+    });
+
+    it("repeated workspace.save() calls fire the datasource.save method only once", function() {
+        sinon.spy(this.ds, "save");
+        const prom1 = this.workspace.save();
+        const prom2 = this.workspace.save();
+        const prom3 = this.workspace.save();
+        expect(prom1).to.equal(prom2);
+        expect(prom2).to.equal(prom3);
+        return prom3.then(() => {
+            expect(this.ds.save.calledOnce).to.be.true;
+        });
+    });
+
+});
