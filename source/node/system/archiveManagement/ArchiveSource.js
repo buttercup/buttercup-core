df72d27f3e26e92c33afc6d52493e8246e313a5b
Perry Mitchell <perry@perrymitchell.net>
2018-02-12 18:45:54 +0200

    Add archive source

---
+++ source/node/system/archiveManagement/ArchiveSource.js
@@ -0,0 +1,149 @@
+const VError = require("verror");
+const AsyncEventEmitter = require("./events/AsyncEventEmitter.js");
+const getUniqueID = require("../tools/encoding.js").getUniqueID;
+const createCredentials = require("./credentials.js");
+const credentialsToSource = require("./archiveManagement/marshalling.js").credentialsToSource;
+
+const DefaultColour = "#000000";
+const Status = {
+    LOCKED: "locked",
+    UNLOCKED: "unlocked",
+    PENDING: "pending"
+};
+
+class ArchiveSource extends AsyncEventEmitter {
+    constructor(name, sourceCredentials, archiveCredentials, id = getUniqueID()) {
+        super();
+        if (createCredentials.isSecureString(sourceCredentials) !== true) {
+            throw new VError("Failed constructing archive source: Source credentials not in encrypted form");
+        }
+        if (createCredentials.isSecureString(archiveCredentials) !== true) {
+            throw new VError("Failed constructing archive source: Archive credentials not in encrypted form");
+        }
+        this._name = name;
+        this._id = id;
+        this._status = Status.LOCKED;
+        this._sourceCredentials = sourceCredentials;
+        this._archiveCredentials = archiveCredentials;
+        this._workspace = null;
+        this._colour = DefaultColour;
+        this.type = "";
+    }
+
+    get colour() {
+        return this._colour;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get status() {
+        return this._status;
+    }
+
+    get workspace() {
+        return this._workspace;
+    }
+
+    set colour(newColour) {
+        if (/^$/.test(newColour) !== true) {
+            throw new VError(`Failed setting colour: Invalid format (expected hex): ${newColour}`);
+        }
+        this._colour = newColour;
+    }
+
+    dehydrate() {
+        if (this.status === Status.PENDING) {
+            return Promise.reject(new VError(`Failed dehydrating source: Source in pending state: ${this.id}`));
+        }
+        return Promise.resolve()
+            .then(() => {
+                const payload = {
+                    id: this.id,
+                    name: this.name,
+                    type: this.type,
+                    status: Status.LOCKED
+                };
+                if (this.status === Status.LOCKED) {
+                    payload.sourceCredentials = this._sourceCredentials;
+                    payload.archiveCredentials = this._archiveCredentials;
+                    return payload;
+                }
+                return Promise.all([
+                    this._sourceCredentials.toSecureString(this._archiveCredentials.password),
+                    this._archiveCredentials.toSecureString(this._archiveCredentials.password)
+                ]).then(([encSourceCredentials, encArchiveCredentials]) => {
+                    payload.sourceCredentials = encSourceCredentials;
+                    payload.archiveCredentials = encArchiveCredentials;
+                    return payload;
+                });
+            })
+            .then(payload => JSON.stringify(payload))
+            .catch(err => {
+                throw new VError(err, `Failed dehydrating source: ${this.id}`);
+            });
+    }
+
+    lock() {
+        if (this.status !== Status.UNLOCKED) {
+            return Promise.reject(
+                new VError(`Failed locking source: Source in invalid state (${this.status}): ${this.id}`)
+            );
+        }
+        this._status = Status.PENDING;
+        return Promise.all([
+            this._sourceCredentials.toSecureString(this._archiveCredentials.password),
+            this._archiveCredentials.toSecureString(this._archiveCredentials.password)
+        ])
+            .then(([encSourceCredentials, encArchiveCredentials]) => {
+                this._status = Status.LOCKED;
+                this._workspace = null;
+                this._sourceCredentials = encSourceCredentials;
+                this._archiveCredentials = encArchiveCredentials;
+                return this.dehydrate();
+            })
+            .catch(err => {
+                throw new VError(err, `Failed locking source: ${this.id}`);
+            });
+    }
+
+    unlock(masterPassword) {
+        if (this.status !== Status.LOCKED) {
+            return Promise.reject(
+                new VError(`Failed unlocking source: Source in invalid state (${this.status}): ${this.id}`)
+            );
+        }
+        this._status = Status.PENDING;
+        return Promise.all([
+            createCredentials.fromSecureString(this._sourceCredentials, masterPassword),
+            createCredentials.fromSecureString(this._archiveCredentials, masterPassword)
+        ])
+            .then(([sourceCredentials, archiveCredentials] = []) => {
+                return credentialsToSource(sourceCredentials, archiveCredentials, /* initialise */ false)
+                    .then(sourceInfo => {
+                        const { workspace, sourceCredentials, archiveCredentials } = sourceInfo;
+                        this._workspace = workspace;
+                        this._sourceCredentials = sourceCredentials;
+                        this._archiveCredentials = archiveCredentials;
+                        this._status = Status.UNLOCKED;
+                        this.type = sourceCredentials.type;
+                    })
+                    .catch(err => {
+                        throw new VError(err, "Failed mapping credentials to a source");
+                    });
+            })
+            .catch(err => {
+                this._status = Status.LOCKED;
+                throw new VError(err, `Failed unlocking source: ${this.id}`);
+            });
+    }
+}
+
+ArchiveSource.Status = Status;
+
+module.exports = ArchiveSource;
