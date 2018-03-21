b98601d614476d56ce99b21871b9c3819d2d674c
Perry Mitchell <perry@perrymitchell.net>
2018-03-21 13:52:18 +0200

    Fix web tests

--- source/node/tools/encoding.js
+++ source/node/tools/encoding.js
@@ -1,4 +1,3 @@
-const gzip = require("gzip-js");
 const { getUUIDGenerator } = require("./uuid.js");
 
 const ENCODED_STRING_PATTERN = /^utf8\+base64:(|[a-zA-Z0-9+\/=]+)$/;
@@ -12,24 +11,6 @@ const lib = (module.exports = {
     ENCODED_STRING_PREFIX,
 
     /**
-     * Compress text using GZIP
-     * @param {String} text The text to compress
-     * @returns {String} Compressed text
-     */
-    compress: function(text) {
-        const compressed = gzip.zip(text, {
-            level: 9,
-            timestamp: parseInt(Date.now() / 1000, 10)
-        });
-        const compressedLength = compressed.length;
-        const outputText = "";
-        for (var i = 0; i < compressedLength; i += 1) {
-            outputText += String.fromCharCode(compressed[i]);
-        }
-        return outputText;
-    },
-
-    /**
      * Decode an encoded property/meta value
      * @param {String} value The encoded value
      * @returns {String} The decoded value
@@ -41,26 +22,6 @@ const lib = (module.exports = {
         const newValue = value.substr(ENCODED_STRING_PREFIX.length);
         const buff = Buffer.from(newValue, "base64");
         return buff.toString("utf8");
-    },
-
-    /**
-     * Decompress a compressed string (GZIP)
-     * @param {String} text The compressed text
-     * @returns {String} Decompressed text
-     */
-    decompress: function(text) {
-        var compressedData = [],
-            textLen = text.length;
-        for (var i = 0; i < textLen; i += 1) {
-            compressedData.push(text.charCodeAt(i));
-        }
-        var decompressedData = gzip.unzip(compressedData),
-            decompressedLength = decompressedData.length,
-            outputText = "";
-        for (var j = 0; j < decompressedLength; j += 1) {
-            outputText += String.fromCharCode(decompressedData[j]);
-        }
-        return outputText;
     },
 
     /**
