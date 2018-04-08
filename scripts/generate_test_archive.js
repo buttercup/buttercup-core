0287cc1160bb72566d0a5214892190a7d3cf8d61
Perry Mitchell <perry@perrymitchell.net>
2018-04-08 20:03:44 +0300

    Reduce iocane iterations for test archive generation

--- scripts/generate_test_archive.js
+++ scripts/generate_test_archive.js
@@ -1,13 +1,15 @@
 const path = require("path");
-const { Archive, Credentials, Datasources } = require("../source/node/index.js");
+const { Archive, Credentials, Datasources, vendor } = require("../source/node/index.js");
 const packageInfo = require("../package.json");
 
 const { FileDatasource } = Datasources;
-
+const { iocane } = vendor;
 const { version } = packageInfo;
 
 const outputDir = path.resolve(__dirname, "../test/resources/archives");
 const outputFile = path.join(outputDir, `/test-archive-${version}.bcup`);
+
+iocane.configure().setDerivationRounds(10);
 
 console.log("Building archive...");
 
