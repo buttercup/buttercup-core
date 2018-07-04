6dab04d29bdcec4a8dac8044f0e987e1191e6147
Perry Mitchell <perry@perrymitchell.net>
2018-07-04 23:11:12 +0300

    Add helpers for URL scraping from entries

--- source/node/tools/entry.js
+++ source/node/tools/entry.js
@@ -1,5 +1,13 @@
 const { objectValues } = require("./polyfill.js");
 
+const ENTRY_URL_TYPE_ANY = "any";
+const ENTRY_URL_TYPE_GENERAL = "general";
+const ENTRY_URL_TYPE_ICON = "icon";
+const ENTRY_URL_TYPE_LOGIN = "login";
+
+const URL_PROP = /(^|[a-zA-Z0-9_-]|\b)(ur[li]|UR[LI]|Ur[li])(\b|$|[_-])/;
+const URL_PROP_ICON = /icon[\s_-]*ur[li]/i;
+
 /**
  * Entry facade data field
  * @typedef {Object} EntryFacadeField
@@ -43,6 +51,58 @@ function createFieldDescriptor(
 }
 
 /**
+ * Get URLs from an entry's propertyies
+ * Allows for preferential sorting
+ * @param {Object} properties The entry properties
+ * @param {*} preference
+ */
+function getEntryURLs(properties, preference = ENTRY_URL_TYPE_ANY) {
+    const urlRef = Object.keys(properties)
+        .filter(key => URL_PROP.test(key))
+        .reduce(
+            (output, nextKey) =>
+                Object.assign(output, {
+                    [nextKey]: properties[nextKey]
+                }),
+            {}
+        );
+    if (preference === ENTRY_URL_TYPE_GENERAL || preference === ENTRY_URL_TYPE_LOGIN) {
+        return Object.keys(urlRef)
+            .sort((a, b) => {
+                if (preference === ENTRY_URL_TYPE_GENERAL) {
+                    const general = /^ur[li]$/i;
+                    const aVal = general.test(a) ? 1 : 0;
+                    const bVal = general.test(b) ? 1 : 0;
+                    return bVal - aVal;
+                } else if (preference === ENTRY_URL_TYPE_LOGIN) {
+                    const login = /login/i;
+                    const aVal = login.test(a) ? 1 : 0;
+                    const bVal = login.test(b) ? 1 : 0;
+                    return bVal - aVal;
+                }
+                return 0;
+            })
+            .map(key => urlRef[key]);
+    } else if (preference === ENTRY_URL_TYPE_ICON) {
+        const iconProp = Object.keys(urlRef).find(key => URL_PROP_ICON.test(key));
+        return iconProp ? [urlRef[iconProp]] : [];
+    }
+    // Default is "any" URLs
+    return objectValues(urlRef);
+}
+
+function getEntryFacadeURLs(facade, preference) {
+    const props = facade.fields.filter(item => item.field === "property").reduce(
+        (output, field) =>
+            Object.assign(output, {
+                [field.property]: field.value
+            }),
+        {}
+    );
+    return getEntryURLs(props, preference);
+}
+
+/**
  * Get a value on an entry for a specific property type
  * @param {Entry} entry The entry instance
  * @param {String} property The type of entry property (property/meta/attribute)
@@ -81,7 +141,13 @@ function isValidProperty(name) {
 }
 
 module.exports = {
+    ENTRY_URL_TYPE_ANY,
+    ENTRY_URL_TYPE_GENERAL,
+    ENTRY_URL_TYPE_ICON,
+    ENTRY_URL_TYPE_LOGIN,
     createFieldDescriptor,
+    getEntryURLs,
+    getEntryFacadeURLs,
     getEntryValue,
     isValidProperty
 };
