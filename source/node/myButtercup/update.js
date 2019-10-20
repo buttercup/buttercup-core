ee1701268bb03057c3e4d94d090ec1a9c3967627
Perry Mitchell <perry@perrymitchell.net>
2019-10-20 16:05:21 +0300

    Add more docs

--- source/node/myButtercup/update.js
+++ source/node/myButtercup/update.js
@@ -1,5 +1,9 @@
 const MAX_INT_VALUE = 2147483647;
 
+/**
+ * Generate a new update ID
+ * @returns {Number} A randomly generated ID
+ */
 function generateNewUpdateID() {
     return Math.floor(Math.random() * MAX_INT_VALUE);
 }
