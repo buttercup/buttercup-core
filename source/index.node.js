579bf8ef2dbe18a8afbcba4330a4d7b8eb675ebd
Perry Mitchell <perry@perrymitchell.net>
2020-03-22 13:35:39 +0200

    Execute app env from indexes

--- source/index.node.js
+++ source/index.node.js
@@ -1,1 +1,7 @@
+const { getSharedAppEnv } = require("./env/core/singleton.js");
+const { applyNativeConfiguration } = require("./env/native/index.js");
+
+const appEnv = getSharedAppEnv();
+applyNativeConfiguration(appEnv);
+
 module.exports = require("./index.common.js");
--- source/index.web.js
+++ source/index.web.js
@@ -1,1 +1,7 @@
+const { getSharedAppEnv } = require("./env/core/singleton.js");
+const { applyWebConfiguration } = require("./env/web/index.js");
+
+const appEnv = getSharedAppEnv();
+applyWebConfiguration(appEnv);
+
 module.exports = require("./index.common.js");
