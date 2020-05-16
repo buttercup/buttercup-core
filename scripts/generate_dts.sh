03cfe08351d2488b6b24c841147f605f8e380ed2
Sallar Kaboli <sallar@kaboli.org>
2020-05-17 01:08:39 +0300

    Fix type building for v4

--- scripts/generate_dts.sh
+++ scripts/generate_dts.sh
@@ -2,4 +2,4 @@
 
 ./node_modules/.bin/tsc 
 sed -i '' 's/.js"/"/g' dist/index.d.ts
-sed -i '' 's/module "index"/module "buttercup"/' dist/index.d.ts
+sed -i '' 's/module "index.common"/module "buttercup"/' dist/index.d.ts
