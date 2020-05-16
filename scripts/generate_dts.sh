f184c79507783a1726a3d74b0f7e7ba79090a723
Sallar Kaboli <sallar@kaboli.org>
2020-05-17 01:24:29 +0300

    Name all exports

--- scripts/generate_dts.sh
+++ scripts/generate_dts.sh
@@ -3,3 +3,5 @@
 ./node_modules/.bin/tsc 
 sed -i '' 's/.js"/"/g' dist/index.d.ts
 sed -i '' 's/module "index.common"/module "buttercup"/' dist/index.d.ts
+sed -i '' 's/module "index.web"/module "buttercup\/web"/' dist/index.d.ts
+sed -i '' 's/module "index.node"/module "buttercup\/node"/' dist/index.d.ts
