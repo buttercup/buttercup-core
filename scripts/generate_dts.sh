7bda0177e4f04f34b7e8696e6cb77525712e9dc7
Perry Mitchell <perry@perrymitchell.net>
2020-05-17 14:50:14 +0300

    Fix types generation

--- scripts/generate_dts.sh
+++ scripts/generate_dts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ./node_modules/.bin/tsc 
-sed -i '' 's/.js"/"/g' dist/index.d.ts
-sed -i '' 's/module "index.common"/module "buttercup"/' dist/index.d.ts
-sed -i '' 's/module "index.web"/module "buttercup\/web"/' dist/index.d.ts
-sed -i '' 's/module "index.node"/module "buttercup\/node"/' dist/index.d.ts
+sed -i 's/.js"/"/g' dist/index.d.ts
+sed -i 's/module "index.common"/module "buttercup"/' dist/index.d.ts
+sed -i 's/module "index.web"/module "buttercup\/web"/' dist/index.d.ts
+sed -i 's/module "index.node"/module "buttercup\/node"/' dist/index.d.ts
