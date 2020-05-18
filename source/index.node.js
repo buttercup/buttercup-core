6aa4fca32575a89f76be2ee6b210cf9d54d370ae
Perry Mitchell <perry@perrymitchell.net>
2020-05-18 22:43:35 +0300

    Improve init: calling multiple times doesn't crash

--- source/index.node.js
+++ source/index.node.js
@@ -1,11 +1,15 @@
 const { getSharedAppEnv } = require("./env/core/singleton.js");
 const { applyNativeConfiguration } = require("./env/native/index.js");
 
+let __hasInitialised = false;
+
 /**
  * Initialise the node/native environment
  * @memberof module:Buttercup
  */
 function init() {
+    if (__hasInitialised) return;
+    __hasInitialised = true;
     const appEnv = getSharedAppEnv();
     applyNativeConfiguration(appEnv);
 }
--- source/index.web.js
+++ source/index.web.js
@@ -1,11 +1,15 @@
 const { getSharedAppEnv } = require("./env/core/singleton.js");
 const { applyWebConfiguration } = require("./env/web/index.js");
 
+let __hasInitialised = false;
+
 /**
  * Initialise the web environment
  * @memberof module:Buttercup
  */
 function init() {
+    if (__hasInitialised) return;
+    __hasInitialised = true;
     const appEnv = getSharedAppEnv();
     applyWebConfiguration(appEnv);
 }
