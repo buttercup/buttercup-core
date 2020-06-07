f4a86d4393cc4a587f73c64d9f89cd5fc920711d
Perry Mitchell <perry@perrymitchell.net>
2020-06-07 20:12:54 +0300

    Update docs for attachment manager

--- source/attachments/AttachmentManager.js
+++ source/attachments/AttachmentManager.js
@@ -1,15 +1,48 @@
 const { v4: uuid } = require("uuid");
 
+/**
+ * @typedef {Object} AttachmentDetails
+ * @property {String} id The attachment ID
+ * @property {String} name The name of the file
+ * @property {String} type The MIME type
+ * @property {Number} size The size of the file, before encryption
+ */
+
+/**
+ * Attachment manager
+ * @memberof module:Buttercup
+ */
 class AttachmentManager {
+    /**
+     * Get a new attachment ID
+     * @returns {String}
+     * @static
+     * @memberof AttachmentManager
+     */
     static newAttachmentID() {
         return uuid();
     }
 
+    /**
+     * Constructor for new attachment managers
+     * @param {VaultSource} vaultSource The vault source to attach to. This is
+     *  normally set by the VaultSource automatically when unlocking a source.
+     * @param {Credentials} credentials The credentials to use for encrypting
+     *  and decrypting attachments.
+     */
     constructor(vaultSource, credentials) {
         this._source = vaultSource;
         this._credentials = credentials;
     }
 
+    /**
+     * Get an attachment's data
+     * @param {Entry} entry Entry instance the attachment can be found on
+     * @param {String} attachmentID The attachment ID
+     * @returns {Promise.<Buffer|ArrayBuffer>}
+     * @throws {Error} Throws if the attachment isn't found
+     * @memberof AttachmentManager
+     */
     async getAttachment(entry, attachmentID) {
         this._checkAttachmentSupport();
         const details = await this.getAttachmentDetails(entry, attachmentID);
@@ -19,6 +52,14 @@ class AttachmentManager {
         return this._source._datasource.getAttachment(this._source.vault.id, attachmentID, this._credentials);
     }
 
+    /**
+     * Get an attachment's details
+     * @param {Entry} entry Entry instance the attachment can be found on
+     * @param {String} attachmentID The attachment ID
+     * @returns {Promise.<AttachmentDetails|null>} The details or null if
+     *  the attachment isn't found
+     * @memberof AttachmentManager
+     */
     async getAttachmentDetails(entry, attachmentID) {
         this._checkAttachmentSupport();
         const Entry = require("../core/Entry.js");
@@ -27,6 +68,12 @@ class AttachmentManager {
         return payloadRaw ? JSON.parse(payloadRaw) : null;
     }
 
+    /**
+     * List all attachments
+     * @param {Entry} entry Entry instance the attachment can be found on
+     * @returns {Promise.<AttachmentDetails[]>}
+     * @memberof AttachmentManager
+     */
     async listAttachments(entry) {
         this._checkAttachmentSupport();
         const Entry = require("../core/Entry.js");
@@ -37,6 +84,14 @@ class AttachmentManager {
         return attachmentKeys.map(key => JSON.parse(attributes[key]));
     }
 
+    /**
+     * Remove an attachment, deleting the file and removing it from
+     *  the entry
+     * @param {Entry} entry Entry instance the attachment can be found on
+     * @param {String} attachmentID The attachment ID
+     * @returns {Promise}
+     * @memberof AttachmentManager
+     */
     async removeAttachment(entry, attachmentID) {
         this._checkAttachmentSupport();
         const Entry = require("../core/Entry.js");
@@ -47,6 +102,18 @@ class AttachmentManager {
         entry.deleteAttribute(attributeKey);
     }
 
+    /**
+     * Write an attachment to an entry.
+     * Creates a new attachment or updates an existing one, by attachment ID
+     * @param {Entry} entry Entry instance the attachment can be found on
+     * @param {String} attachmentID The attachment ID
+     * @param {Buffer|ArrayBuffer} attachmentData The attachment's data
+     * @param {String} name The file name
+     * @param {String} type The MIME type
+     * @param {Number} size The byte size of the attachment, before encryption
+     * @returns {Promise}
+     * @memberof AttachmentManager
+     */
     async setAttachment(entry, attachmentID, attachmentData, name, type, size) {
         this._checkAttachmentSupport();
         if (!name || !type || !size) {
