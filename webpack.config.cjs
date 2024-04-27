7a9895ba15971665860d2177c63e36df94e236e5
Perry Mitchell <perry@perrymitchell.net>
2024-04-27 15:44:54 +0300

    Start configuring RN build

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,12 +1,13 @@
 const path = require("path");
-const exec = require("child_process").exec;
 const { DefinePlugin } = require("webpack");
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const ResolveTypeScriptPlugin = require("resolve-typescript-plugin");
+const { merge } = require("webpack-merge");
 
 const SOURCE = path.resolve(__dirname, "./source");
 const WEB_ENTRY = path.join(SOURCE, "index.web.ts");
-const DIST = path.resolve(__dirname, "./dist/web");
+const WEB_DIST = path.resolve(__dirname, "./dist/web");
+const RN_DIST = path.resolve(__dirname, "./dist/react-native");
 
 const plugins = [
     new DefinePlugin({
@@ -17,69 +18,85 @@ if (process.env.ANALYSE === "bundle") {
     plugins.push(new BundleAnalyzerPlugin());
 }
 
-const baseConfig = {
-    devtool: false,
+function getBasicConfig() {
+    return {
+        devtool: false,
 
-    entry: WEB_ENTRY,
+        experiments: {
+            outputModule: true
+        },
 
-    experiments: {
-        outputModule: true
-    },
+        externalsType: "module",
 
-    externalsType: "module",
-
-    module: {
-        rules: [
-            {
-                test: /(iconv-loader|bluebird|form-data)/,
-                use: "null-loader"
-            },
-            {
-                test: /\.ts$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "ts-loader",
-                    options: {
-                        configFile: path.resolve(__dirname, "./tsconfig.web.json"),
-                        onlyCompileBundledFiles: true
+        module: {
+            rules: [
+                {
+                    test: /(iconv-loader|bluebird|form-data)/,
+                    use: "null-loader"
+                },
+                {
+                    test: /\.ts$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "ts-loader",
+                        options: {
+                            configFile: path.resolve(__dirname, "./tsconfig.web.json"),
+                            onlyCompileBundledFiles: true
+                        }
                     }
                 }
+            ]
+        },
+
+        output: {
+            filename: "index.js",
+            environment: {
+                module: true
+            },
+            library: {
+                type: "module"
             }
-        ]
-    },
-
-    output: {
-        path: DIST,
-        filename: "index.js",
-        environment: {
-            module: true
         },
-        library: {
-            type: "module"
+
+        plugins,
+
+        resolve: {
+            extensions: [".js"],
+            fallback: {
+                crypto: false,
+                dns: false,
+                fs: false,
+                http: false,
+                https: false,
+                net: false,
+                path: false,
+                stream: false,
+                util: false
+            },
+            plugins: [
+                new ResolveTypeScriptPlugin()
+            ]
         }
-    },
+    };
+}
 
-    plugins,
+module.exports = [
+    merge(getBasicConfig(), {
+        entry: WEB_ENTRY,
 
-    resolve: {
-        extensions: [".js"],
-        fallback: {
-            crypto: false,
-            dns: false,
-            fs: false,
-            http: false,
-            https: false,
-            net: false,
-            path: false,
-            stream: false,
-            util: false
+        output: {
+            path: WEB_DIST
         },
-        plugins: [
-            new ResolveTypeScriptPlugin()
-        ]
-    },
+    
+        target: "web"
+    }),
+    merge(getBasicConfig(), {
+        entry: WEB_ENTRY,
 
-    target: "web"
-};
-
-module.exports = baseConfig;
+        output: {
+            path: RN_DIST
+        },
+    
+        target: "web"
+    })
+];
